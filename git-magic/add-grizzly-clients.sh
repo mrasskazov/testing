d6db710f91ae472b497a126199f5ad82d300054f
Yuriy Taraday <yorik.sar@gmail.com>
2013-04-23 15:32:58 +0400

    Update add-grizzly-clients.sh to use mirrors.
    Change-Id: I263b2859fd735c9922e2dfa1dea738bef6fc052f

--- git-magic/add-grizzly-clients.sh
+++ git-magic/add-grizzly-clients.sh
@@ -1,22 +1,22 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 . subr
 
 export USERNAME=ytaraday
+PROJECTS=" oslo.config "
 add_client_projects
 TARGET_VERSION=grizzly
+FUEL_VERSION=2.1
+init_repo
 
 for prj in $PROJECTS; do
     add_mirantis_remote
-    add_github_remote
     cleanup_tags
-    git fetch -q --tags github-$prj
-    git fetch -q github-$prj master
+    git fetch -q --tags mirantis-$prj
+    git fetch -q mirantis-$prj master
     tag_base=$(git describe --tags FETCH_HEAD)
     tag_base=${tag_base%%-*}
-    set -x
     git push mirantis-$prj $tag_base^0:refs/heads/openstack-ci/$TARGET_VERSION
     git push mirantis-$prj $tag_base^0:refs/tags/openstack-ci/$tag_base
-    git push mirantis-$prj $tag_base^0:refs/tags/openstack-ci/fuel/$tag_base/2.1
-    set +x
+    git push mirantis-$prj $tag_base^0:refs/tags/openstack-ci/fuel/$tag_base/$FUEL_VERSION
     cleanup_all
 done
