d641f095e04bff24a73c40288f268bb2f5e7a136
Max Rasskazov <mrasskazov@mirantis.com>
2013-05-14 13:35:01 +0400

    Config by system environment
    Change-Id: Ie772b388ff8b2c8f7f8a18c75858feb86e0ca2e7

--- tempest-runner/tempest-runner.sh
+++ tempest-runner/tempest-runner.sh
@@ -1,52 +1,5 @@
 #!/bin/bash -x
 
-TOS_REINSTALL_VIRTUALENV=${TOS_REINSTALL_VIRTUALENV:-false}
-TOS_CREATE_ENTITIES=${TOS_CREATE_ENTITIES:-true}
-TOS_DELETE_ENTITIES=${TOS_DELETE_ENTITIES:-false}
-
-TESTCASE=${TESTCASE:-tempest/tests}
-OS_USERNAME=${OS_USERNAME:-admin}
-OS_PASSWORD=${OS_PASSWORD:-secrete}
-OS_TENANT_NAME=${OS_TENANT_NAME:-admin}
-OS_AUTH_STRATEGY=${OS_AUTH_STRATEGY:-keystone}
-
-TOS_IMAGE_NAME=${TOS_IMAGE_NAME:-tempest-cirros-01}
-
-CIRROS_HOST=http://download.cirros-cloud.net/
-wget -c $CIRROS_HOST/version/released
-CIRROS_RELEASE=$(cat released)
-rm released
-TOS_IMAGE_LINK=${TOS_IMAGE_LINK:-"http://download.cirros-cloud.net/$CIRROS_RELEASE/cirros-$CIRROS_RELEASE-x86_64-disk.img"}
-
-TOS_IMAGE_NAME_ALT=${TOS_IMAGE_NAME_ALT:-tempest-cirros-02}
-TOS_IMAGE_LINK_ALT=${TOS_IMAGE_LINK_ALT:-$TOS_IMAGE_LINK}
-
-TOS_FLAVOR_REF=${TOS_FLAVOR_REF:-641}
-TOS_FLAVOR_REF_ALT=${TOS_FLAVOR_REF_ALT:-642}
-
-TOS_MEMBER_ROLE_NAME=${TOS_MEMBER_ROLE_NAME:-Member}
-
-TOS_USERNAME=${TOS_USERNAME:-u1}
-TOS_PASSWORD=${TOS_PASSWORD:-secrete}
-TOS_TENANT_NAME=${TOS_TENANT_NAME:-t1}
-
-TOS_USERNAME_ALT=${TOS_USERNAME_ALT:-u2}
-TOS_PASSWORD_ALT=${TOS_PASSWORD_ALT:-secrete}
-TOS_TENANT_NAME_ALT=${TOS_TENANT_NAME_ALT:-t2}
-
-TOS_PATH_TO_PRIVATE_KEY=${TOS_PATH_TO_PRIVATE_KEY:-/var/lib/puppet/ssh_keys/openstack}
-TOS_DB_HA_HOST=${TOS_DB_HA_HOST:-localhost}
-TOS_DB_URI=${TOS_DB_URI:-mysql://nova:nova@$TOS_DB_HA_HOST/nova}
-
-TOS_RUN_SSH=${TOS_RUN_SSH:-false}
-TOS_SSH_USER=${TOS_SSH_USER:-cirros}
-TOS_NETWORK_FOR_SSH=${TOS_NETWORK_FOR_SSH:-t1_router_net}
-TOS_IP_VERSION_FOR_SSH=${TOS_IP_VERSION_FOR_SSH:-4}
-
-
-TOS__NETWORK__API_VERSION=${TOS__NETWORK__API_VERSION:-v2.0}
-
-
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 TEMPEST_DIR=$(readlink -f $TOP_DIR/../../tempest)
 if [[ ! -r $TEMPEST_DIR ]]; then
@@ -58,17 +11,78 @@ if [ -z $OS_AUTH_URL ]; then
     echo 'ERROR: Missing $OS_AUTH_URL variable'
     exit 2
 fi
-TOS_HOST=$(echo $OS_AUTH_URL | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
-TOS_PORT=$(echo $OS_AUTH_URL | grep -Eo ':([0-9]{1,5})' | cut -d ":" -f2)
-TOS_API_VERSION=$(echo $OS_AUTH_URL | grep -Eo 'v([0-9\.])+')
+
+TESTCASE=${TESTCASE:-tempest/tests}
+OS_USERNAME=${OS_USERNAME:-admin}
+OS_PASSWORD=${OS_PASSWORD:-secrete}
+OS_TENANT_NAME=${OS_TENANT_NAME:-admin}
+OS_AUTH_STRATEGY=${OS_AUTH_STRATEGY:-keystone}
+
+REINSTALL_VIRTUALENV=${REINSTALL_VIRTUALENV:-false}
+CREATE_ENTITIES=${CREATE_ENTITIES:-true}
+DELETE_ENTITIES=${DELETE_ENTITIES:-false}
+
+# detect/define some parameters
+CIRROS_HOST=http://download.cirros-cloud.net/
+CIRROS_RELEASE=$(wget -qO- $CIRROS_HOST/version/released)
+IMAGE_LINK=${IMAGE_LINK:-"$CIRROS_HOST/$CIRROS_RELEASE/cirros-$CIRROS_RELEASE-x86_64-disk.img"}
+IMAGE_NAME=${IMAGE_NAME:-tempest-cirros-01}
+IMAGE_LINK_ALT=${IMAGE_LINK_ALT:-$IMAGE_LINK}
+IMAGE_NAME_ALT=${IMAGE_NAME_ALT:-tempest-cirros-02}
+
+MEMBER_ROLE_NAME=${MEMBER_ROLE_NAME:-Member}
+DB_HA_HOST=${DB_HA_HOST:-localhost}
+
+### DEFAULT CONFIG PARAMETERS ###
+# format: TOS__SECTION_NAME__PARAMETER_NAME
+# SECTION_NAME equals 'EVERYWHERE' means that uses all sections with this parameter
+export TOS__EVERYWHERE__HOST=$(echo $OS_AUTH_URL | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
+export TOS__IDENTITY__PORT=$(echo $OS_AUTH_URL | grep -Eo ':([0-9]{1,5})' | cut -d ":" -f2)
+export TOS__IDENTITY__API_VERSION=$(echo $OS_AUTH_URL | grep -Eo 'v([0-9\.])+')
+export TOS__IDENTITY__STRATEGY=${OS_AUTH_STRATEGY}
+
+export TOS__COMPUTE__USERNAME=${TOS__COMPUTE__USERNAME:-u1}
+export TOS__COMPUTE__PASSWORD=${TOS__COMPUTE__PASSWORD:-secrete}
+export TOS__COMPUTE__TENANT_NAME=${TOS__COMPUTE__TENANT_NAME:-t1}
+
+export TOS__COMPUTE__ALT_USERNAME=${TOS__COMPUTE__ALT_USERNAME:-u2}
+export TOS__COMPUTE__ALT_PASSWORD=${TOS__COMPUTE__ALT_PASSWORD:-secrete}
+export TOS__COMPUTE__ALT_TENANT_NAME=${TOS__COMPUTE__ALT_TENANT_NAME:-t2}
+
+export TOS__COMPUTE__FLAVOR_REF=${TOS__COMPUTE__FLAVOR_REF:-641}
+export TOS__COMPUTE__FLAVOR_REF_ALT=${TOS__COMPUTE__FLAVOR_REF_ALT:-642}
+
+export TOS__COMPUTE__PATH_TO_PRIVATE_KEY=${TOS__COMPUTE__PATH_TO_PRIVATE_KEY:-/var/lib/puppet/ssh_keys/openstack}
+export TOS__COMPUTE__RUN_SSH=${TOS__COMPUTE__RUN_SSH:-false}
+export TOS__COMPUTE__SSH_USER=${TOS__COMPUTE__SSH_USER:-cirros}
+export TOS__COMPUTE__NETWORK_FOR_SSH=${TOS__COMPUTE__NETWORK_FOR_SSH:-t1_router_net}
+export TOS__COMPUTE__IP_VERSION_FOR_SSH=${TOS__COMPUTE__IP_VERSION_FOR_SSH:-4}
+
+export TOS__COMPUTE__DB_URI=${TOS__COMPUTE__DB_URI:-mysql://nova:nova@$DB_HA_HOST/nova}
+
+export TOS__IMAGE__USERNAME=${TOS__IMAGE__USERNAME:-${TOS__COMPUTE__USERNAME}}
+export TOS__IMAGE__PASSWORD=${TOS__IMAGE__PASSWORD:-${TOS__COMPUTE__PASSWORD}}
+export TOS__IMAGE__TENANT_NAME=${TOS__IMAGE__TENANT_NAME:-${TOS__COMPUTE__TENANT_NAME}}
+
+export TOS__COMPUTE_ADMIN__USERNAME=${TOS__COMPUTE_ADMIN__USERNAME:-${OS_USERNAME}}
+export TOS__COMPUTE_ADMIN__PASSWORD=${TOS__COMPUTE_ADMIN__PASSWORD:-${OS_PASSWORD}}
+export TOS__COMPUTE_ADMIN__TENANT_NAME=${TOS__COMPUTE_ADMIN__TENANT_NAME:-${OS_TENANT_NAME}}
+
+export TOS__NETWORK__API_VERSION=${TOS__NETWORK__API_VERSION:-v2.0}
+
+export TOS__IDENTITY_ADMIN__USERNAME=${TOS__IDENTITY_ADMIN__USERNAME:-${OS_USERNAME}}
+export TOS__IDENTITY_ADMIN__PASSWORD=${TOS__IDENTITY_ADMIN__PASSWORD:-${OS_PASSWORD}}
+export TOS__IDENTITY_ADMIN__TENANT_NAME=${TOS__IDENTITY_ADMIN__TENANT_NAME:-${OS_TENANT_NAME}}
 
 
-change_value () {
+ini_param () {
+    # TODO: error with not founded section/parameter needed.
+    # or creating it if $5==CREATE
     FILENAME=$1
     SECTION=$2
     PARAM=$3
     VALUE=$4
-    if [ "$SECTION" == "__ANY__" ]; then
+    if [ "$SECTION" == "everywhere" ]; then
         # change all the matching
         sed -i -e "s|\(^$PARAM\s=\).*|\1 $VALUE|" $FILENAME
     else
@@ -96,6 +110,7 @@ tenant_create () {
         echo $?
     fi
 }
+
 user_create () {
     # user_name tenant_name password email enabled
     if [ "$(get_id $1 keystone user-list)" == "" ]; then
@@ -103,6 +118,7 @@ user_create () {
         echo $?
     fi
 }
+
 user_role_add () {
     # user_name tenant_name role_name
     if [ "$(get_id $3 keystone role-list)" != "" ]; then
@@ -111,9 +127,10 @@ user_role_add () {
             echo $?
         fi
     else
-        echo "Wrong system variable value: \$TOS_MEMBER_ROLE_NAME=$TOS_MEMBER_ROLE_NAME"
+        echo "Wrong system variable value: \$MEMBER_ROLE_NAME=$MEMBER_ROLE_NAME"
     fi
 }
+
 flavor_create () {
     # is_public flavor_name id ram disk vcpus
     if [ "$(get_id $2 nova flavor-list)" == "" ]; then
@@ -121,6 +138,7 @@ flavor_create () {
         echo $?
     fi
 }
+
 image_create_img () {
     # is-public name IMANE_LINK disk-format container-format
     if [ "$(get_id $2 glance image-list)" == "" ]; then
@@ -130,6 +148,7 @@ image_create_img () {
         echo $?
     fi
 }
+
 net_create () {
     # tenant_name sip_lease gateway eg_id net_type ip_version router_name net_name subnet_name
     # if [ "$(get_id $2 glance image-list)" == "" ]; then
@@ -167,6 +186,7 @@ net_create () {
     # for external network
     #quantum router-gateway-set --request-format json $ROUTER_ID $NET_ID
 }
+
 net_delete () {
     # tenant_name router_name net_name subnet_name
     # if [ "$(get_id $2 glance image-list)" == "" ]; then
@@ -188,126 +208,99 @@ net_delete () {
 }
 
 
-pushd $TEMPEST_DIR
+pushd $TOP_DIR/../..
     echo "================================================================================="
     route -n
     echo "================================================================================="
     find . -name *.pyc -delete
-    if [ "$TOS_REINSTALL_VIRTUALENV" = "true" ]; then
-        rm -rf .tempest_venv
+    if [ "$REINSTALL_VIRTUALENV" = "true" ]; then
+        rm -rf venv_tempest
     fi
-    virtualenv .tempest_venv
-    . .tempest_venv/bin/activate
-    pip install -r tools/pip-requires || exit 1
-    pip install -r tools/test-requires || exit 1
+    virtualenv venv_tempest
+    . venv_tempest/bin/activate
     pip install -r $TOP_DIR/tempest-runner-pre-requires || exit 1
+    pip install -r $TEMPEST_DIR/tools/pip-requires || exit 1
+    pip install -r $TEMPEST_DIR/tools/test-requires || exit 1
     pip install -r $TOP_DIR/tempest-runner-requires || exit 1
 
-    if [ "$TOS_CREATE_ENTITIES" = "true" ]; then
+    if [ "$CREATE_ENTITIES" = "true" ]; then
         echo "================================================================================="
         echo "Preparing Tempest's environment..."
 
-        tenant_create $TOS_TENANT_NAME
-        user_create $TOS_USERNAME $TOS_TENANT_NAME $TOS_PASSWORD $TOS_USERNAME@$TOS_TENANT_NAME.qa true
-        user_role_add $TOS_USERNAME $TOS_TENANT_NAME $TOS_MEMBER_ROLE_NAME
-        net_create $TOS_TENANT_NAME 10.0.1.0/24
+        tenant_create $TOS__COMPUTE__TENANT_NAME
+        user_create $TOS__COMPUTE__USERNAME $TOS__COMPUTE__TENANT_NAME $TOS__COMPUTE__PASSWORD $TOS__COMPUTE__USERNAME@$TOS_TENANT_NAME.qa true
+        user_role_add $TOS__COMPUTE__USERNAME $TOS__COMPUTE__TENANT_NAME $MEMBER_ROLE_NAME
+        net_create $TOS__COMPUTE__TENANT_NAME 10.0.1.0/24
 
-        tenant_create $TOS_TENANT_NAME_ALT
-        user_create $TOS_USERNAME_ALT $TOS_TENANT_NAME_ALT $TOS_PASSWORD_ALT $TOS_USERNAME_ALT@$TOS_TENANT_NAME_ALT.qa true
-        user_role_add $TOS_USERNAME_ALT $TOS_TENANT_NAME_ALT $TOS_MEMBER_ROLE_NAME
-        net_create $TOS_TENANT_NAME_ALT 10.0.2.0/24
+        tenant_create $TOS__COMPUTE__ALT_TENANT_NAME
+        user_create $TOS__COMPUTE__ALT_USERNAME $TOS__COMPUTE__ALT_TENANT_NAME $TOS__COMPUTE__ALT_PASSWORD $TOS__COMPUTE__ALT_USERNAME@$TOS_TENANT_NAME_ALT.qa true
+        user_role_add $TOS__COMPUTE__ALT_USERNAME $TOS__COMPUTE__ALT_TENANT_NAME $MEMBER_ROLE_NAME
+        net_create $TOS__COMPUTE__ALT_TENANT_NAME 10.0.2.0/24
 
-        flavor_create true f64_1 $TOS_FLAVOR_REF 64 0 1
-        flavor_create true f64_2 $TOS_FLAVOR_REF_ALT 64 0 1
+        flavor_create true f64_1 $TOS__COMPUTE__FLAVOR_REF 64 0 1
+        flavor_create true f64_2 $TOS__COMPUTE__FLAVOR_REF_ALT 64 0 1
 
         # is-public name disk-format IMAGE_LINK container-format
-        image_create_img true $TOS_IMAGE_NAME $TOS_IMAGE_LINK qcow2 bare
-        image_create_img true $TOS_IMAGE_NAME_ALT $TOS_IMAGE_LINK qcow2 bare
+        image_create_img true $IMAGE_NAME $IMAGE_LINK qcow2 bare
+        export TOS__COMPUTE__IMAGE_REF=${TOS__COMPUTE__IMAGE_REF:-$(get_id $IMAGE_NAME glance image-list)}
+        image_create_img true $IMAGE_NAME_ALT $IMAGE_LINK_ALT qcow2 bare
+        export TOS__COMPUTE__IMAGE_REF_ALT=${TOS__COMPUTE__IMAGE_REF_ALT:-$(get_id $IMAGE_NAME_ALT glance image-list)}
 
     fi
 
     echo "================================================================================="
     echo "Generating Tempest's config..."
-    pushd etc
-        ORIG=tempest.conf.sample
-        CONF=tempest.conf
-        cp -f $ORIG $CONF
+    pushd $TEMPEST_DIR
+        pushd etc
+            ORIG=tempest.conf.sample
+            CONF=tempest.conf
+            cp -f $ORIG $CONF
 
-        change_value $CONF __ANY__ host $TOS_HOST
-        change_value $CONF identity port $TOS_PORT
-        change_value $CONF identity api_version $TOS_API_VERSION
-        change_value $CONF identity strategy $OS_AUTH_STRATEGY
+            env | grep ^TOS_
+            #env | awk -F '__' '/^TOS__/ {s=$2; gsub("_","-",s); v=split($3,kv,"="); end; print tolower(s" "kv[1])" "kv[2]}' | xargs --verbose -n3 -i% ini_param $CONF %
+            for p in $(env | awk -F '__' '/^TOS__/ {s=$2; gsub("_","-",s); v=split($3,kv,"="); end; print tolower(s"="kv[1])"="kv[2]}')
+            do
+                sec=$(echo $p | cut -d "=" -f1)
+                par=$(echo $p | cut -d "=" -f2)
+                val=$(echo $p | cut -d "=" -f3)
+                ini_param $CONF $sec $par $val
+            done
+        popd
 
-        change_value $CONF __ANY__ image_ref $(get_id $TOS_IMAGE_NAME glance image-list)
-        change_value $CONF __ANY__ image_ref_alt $(get_id $TOS_IMAGE_NAME_ALT glance image-list)
-        change_value $CONF __ANY__ flavor_ref $TOS_FLAVOR_REF
-        change_value $CONF __ANY__ flavor_ref_alt $TOS_FLAVOR_REF_ALT
+        #export PYTHONPATH="$PWD/tempest:$PYTHONPATH"
 
-        change_value $CONF compute username $TOS_USERNAME
-        change_value $CONF compute password $TOS_PASSWORD
-        change_value $CONF compute tenant_name $TOS_TENANT_NAME
-
-        change_value $CONF compute alt_username $TOS_USERNAME_ALT
-        change_value $CONF compute alt_password $TOS_PASSWORD_ALT
-        change_value $CONF compute alt_tenant_name $TOS_TENANT_NAME_ALT
-
-        change_value $CONF compute path_to_private_key $TOS_PATH_TO_PRIVATE_KEY
-        change_value $CONF compute db_uri $TOS_DB_URI
-
-        change_value $CONF compute run_ssh $TOS_RUN_SSH
-        change_value $CONF compute ssh_user $TOS_SSH_USER
-        change_value $CONF compute network_for_ssh $TOS_NETWORK_FOR_SSH
-        change_value $CONF compute ip_version_for_ssh $TOS_IP_VERSION_FOR_SSH
-
-        change_value $CONF image username $TOS_USERNAME
-        change_value $CONF image password $TOS_PASSWORD
-        change_value $CONF image tenant_name $TOS_TENANT_NAME
-
-        change_value $CONF compute-admin username $OS_USERNAME
-        change_value $CONF compute-admin password $OS_PASSWORD
-        change_value $CONF compute-admin tenant_name $OS_TENANT_NAME
-
-        change_value $CONF identity-admin username $OS_USERNAME
-        change_value $CONF identity-admin password $OS_PASSWORD
-        change_value $CONF identity-admin tenant_name $OS_TENANT_NAME
-
-        change_value $CONF network api_version $TOS__NETWORK__API_VERSION
-    popd
-
-    #export PYTHONPATH="$PWD/tempest:$PYTHONPATH"
-
-    echo "================================================================================="
-    echo "Running tempest..."
-    nosetests -s -v --with-xunit --xunit-file=nosetests.xml $TESTCASE
-    TEMPEST_RET=$?
-
-    if [ "$TOS_DELETE_ENTITIES" = "true" ]; then
         echo "================================================================================="
-        echo "Clean Tempest's environment..."
-        glance image-delete $(get_id $TOS_IMAGE_NAME glance image-list)
-        glance image-delete $(get_id $TOS_IMAGE_NAME_ALT glance image-list)
-        nova flavor-delete $TOS_FLAVOR_REF
-        nova flavor-delete $TOS_FLAVOR_REF_ALT
-        keystone user-role-remove --user-id $(get_id $TOS_USERNAME keystone user-list) --role-id $(get_id $TOS_MEMBER_ROLE_NAME keystone role-list) --tenant-id $(get_id $TOS_TENANT_NAME keystone tenant-list)
-        keystone user-role-remove --user-id $(get_id $TOS_USERNAME_ALT keystone user-list) --role-id $(get_id $TOS_MEMBER_ROLE_NAME keystone role-list) --tenant-id $(get_id $TOS_TENANT_NAME_ALT keystone tenant-list)
-        keystone user-delete $(get_id $TOS_USERNAME keystone user-list)
-        keystone user-delete $(get_id $TOS_USERNAME_ALT keystone user-list)
-        net_delete $TOS_TENANT_NAME
-        net_delete $TOS_TENANT_NAME_ALT
-        keystone tenant-delete $(get_id $TOS_TENANT_NAME keystone tenant-list)
-        keystone tenant-delete $(get_id $TOS_TENANT_NAME_ALT keystone tenant-list)
-    fi
+        echo "Running tempest..."
+        nosetests -s -v --with-xunit --xunit-file=nosetests.xml $TESTCASE
+        TEMPEST_RET=$?
 
-    XML_FILE=$TEMPEST_DIR/nosetests_table.xml
-    cp $TEMPEST_DIR/nosetests.xml $XML_FILE
-    XSL_FILE=xunit.xsl
-    cp $TOP_DIR/$XSL_FILE $TEMPEST_DIR/
-    if [ "$(grep -Eo "xml-stylesheet" $XML_FILE)" == "" ]; then
-        sed -ie "0,/\?></ s/?></?><?xml-stylesheet type=\"text\/xsl\" href=\"$XSL_FILE\"?></" "$XML_FILE"
-    fi
+        if [ "$DELETE_ENTITIES" = "true" ]; then
+            echo "================================================================================="
+            echo "Clean Tempest's environment..."
+            glance image-delete $(get_id $IMAGE_NAME glance image-list)
+            glance image-delete $(get_id $IMAGE_NAME_ALT glance image-list)
+            nova flavor-delete $TOS__COMPUTE__FLAVOR_REF
+            nova flavor-delete $TOS__COMPUTE__FLAVOR_REF_ALT
+            keystone user-role-remove --user-id $(get_id $TOS__COMPUTE__USERNAME keystone user-list) --role-id $(get_id $MEMBER_ROLE_NAME keystone role-list) --tenant-id $(get_id $TOS__COMPUTE__TENANT_NAME keystone tenant-list)
+            keystone user-role-remove --user-id $(get_id $TOS__COMPUTE__ALT_USERNAME keystone user-list) --role-id $(get_id $MEMBER_ROLE_NAME keystone role-list) --tenant-id $(get_id $TOS__COMPUTE__ALT_TENANT_NAME keystone tenant-list)
+            keystone user-delete $(get_id $TOS__COMPUTE__USERNAME keystone user-list)
+            keystone user-delete $(get_id $TOS__COMPUTE__ALT_USERNAME keystone user-list)
+            net_delete $TOS__COMPUTE__TENANT_NAME
+            net_delete $TOS__COMPUTE__ALT_TENANT_NAME
+            keystone tenant-delete $(get_id $TOS__COMPUTE__TENANT_NAME keystone tenant-list)
+            keystone tenant-delete $(get_id $TOS__COMPUTE__ALT_TENANT_NAME keystone tenant-list)
+        fi
 
-    deactivate
+        XML_FILE=$TEMPEST_DIR/nosetests_table.xml
+        cp $TEMPEST_DIR/nosetests.xml $XML_FILE
+        XSL_FILE=xunit.xsl
+        cp $TOP_DIR/$XSL_FILE $TEMPEST_DIR/
+        if [ "$(grep -Eo "xml-stylesheet" $XML_FILE)" == "" ]; then
+            sed -ie "0,/\?></ s/?></?><?xml-stylesheet type=\"text\/xsl\" href=\"$XSL_FILE\"?></" "$XML_FILE"
+        fi
 
+        deactivate
+    popd
 popd
 exit $TEMPEST_RET
 
