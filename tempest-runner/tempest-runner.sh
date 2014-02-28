57490d403238b7510300510375a32b5acbad340b
Max Rasskazov <mrasskazov@mirantis.com>
2014-02-28 18:04:39 +0400

    Added configuration for cli tests
    Change-Id: I00f15721c9738256f86ae8686b4ad5ce3793921b

--- tempest-runner/tempest-runner.sh
+++ tempest-runner/tempest-runner.sh
@@ -551,10 +551,6 @@ pushd $TOP_DIR/../..
     #detect_tempest_release
     source $TOP_DIR/rc.${OS_RELEASE}
 
-    for CLIENT in nova cinder glance keystone; do
-        sudo ln -s $(which $CLIENT) /usr/local/bin/
-    done
-
     pushd $TEMPEST_DIR
 
 
